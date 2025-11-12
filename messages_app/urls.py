f3b39a98ac4a899a3b550970c441bd2953d92967
Milosz <miloszjasica1@gmail.com>
2025-11-12 17:32:44 +0100

    improved view of conversations panel

--- messages_app/urls.py
+++ messages_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('conversation/<int:user_id>/', views.conversation, name='conversation'),
     path('conversation_json/<int:user_id>/', views.conversation_json, name='conversation_json'),
 ]
